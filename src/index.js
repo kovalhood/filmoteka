20f9b94daf3cc3e40f2b1f64f2799112eb3465ba
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-04-07 14:05:35 +0300

    First-Setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+import '../src/js/header.js';
+import '../src/js/fetch-search.js';
+import '../src/js/search-results.js';
+import '../src/js/pagination.js';
+import '../src/js/footer.js';
+import '../src/js/modal.js';
