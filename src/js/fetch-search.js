136513df785bb6385411b5d645bce41c124c358e
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-04-08 15:42:26 +0200

    черновик

--- src/js/fetch-search.js
+++ src/js/fetch-search.js
@@ -1,1 +1,43 @@
 // API_KEY = '387a2500e741e87c896db50117c25d75';
+
+// https://api.themoviedb.org/3/search/movie?api_key=387a2500e741e87c896db50117c25d75&query=Jack+Reacher
+
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+const API_KEY = '387a2500e741e87c896db50117c25d75';
+const options = {
+  headers: {
+    Authorization: API_KEY,
+  },
+};
+
+export default class MoviesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchMovies() {
+    const url = `${BASE_URL}?api_key=${API_KEY}&language=en-US&page=${this.page}&query=${this.searchQuery}`;
+
+    const response = await fetch(url, options);
+    const { movies } = await response.json();
+    this.incrementPage();
+    return movies;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
