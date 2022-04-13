81d5ffab09550bebc4663a5100d6112d0d8ed5f6
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-04-14 00:24:01 +0300

    new version of the js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,229 +1,152 @@
-// const Pagination = require('tui-pagination');
 
-// const containerPag = document.querySelector('pagination');
-// const options = { // below default value of options
-//      totalItems: 10,
-//      itemsPerPage: 10,
-//      visiblePages: 10,
-//      page: 1,
-//      centerAlign: false,
-//      firstItemClassName: 'tui-first-child',
-//      lastItemClassName: 'tui-last-child',
-//      template: {
-//          page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-//          currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-//          moveButton:
-//              '<a href="#" class="tui-page-btn tui-{{type}}">' +
-//                  '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//              '</a>',
-//          disabledMoveButton:
-//              '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-//                  '<span class="tui-ico-{{type}}">{{type}}</span>' +
-//              '</span>',
-//          moreButton:
-//              '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-//                  '<span class="tui-ico-ellip">...</span>' +
-//              '</a>'
-//      }
-// };
-// const pagination = new Pagination(String, Object);
+const Pagination = {
 
+    code: '',
 
-// const movies = [''];
+    // converting initialize data
+    Extend: function(data) {
+        data = data || {};
+        Pagination.size = data.size || 300;
+        Pagination.page = data.page || 1;
+      Pagination.step = data.step || 3;
+    },
 
-// const list_element = document.querySelectorAll('.movies');
-// const pagination_element = document.getElementById('pagination');
-// let current_page = 1;
-// let rows = 5;
+    // add pages by number (from [s] to [f])
+    Add: function(s, f) {
+        for (let i = s; i < f; i++) {
+            Pagination.code += '<a>' + i + '</a>';
+        }
+    },
 
-// function DisplayList(items, wrapper, rows_per_page, page) {
-//     wrapper.innerHTML = "";
-//     page--;
-//     let start = rows_per_page * page;
-//     let end = start + rows_per_page;
-//     let paginatedItems = items.slice(start, end);
-    
-//     for (let i = 0; i < paginatedItems.length; i++) {
-//         let item = paginatedItems[i];
-//         let item_element = document.createElement('div');
-//         item_element.classList.add('item');
-//         item_element.innerText = item;
-//         wrapper.appendChild(item_element);
-//     }
-// }
+    // add last page with separator
+    Last: function() {
+        Pagination.code += '<i>...</i><a>' + Pagination.size + '</a>';
+    },
 
-// function SetupPagination(items, wrapper, rows_per_page) {
-//     wrapper.innerHTML = "";
-//     let page_count = Math.ceil(items.length / rows_per_page);
-//     for (let i = 1; i < page_count + 1; i++){
-//         let btn = PaginationButton(i, items);
-//         wrapper.appendChild(btn)
-//     }
-// }
+    // add first page with separator
+    First: function() {
+        Pagination.code += '<a>1</a><i>...</i>';
+    },
 
-// function PaginationButton(page, items) {
-//     let button = document.createElement('button');
-//     button.innerText = page;
-//     if (current_page == page)
-//         button.classList.add('active');
-//     button.addEventListener('click', function () {
-//         current_page = page;
-//         DisplayList(items, list_element, rows, current_page);
-//         let current_btn = document.querySelector('.pagenumbers button.active');
-//         current_btn.classList.remove('active');
-//         button.classList.add('active');
-//     });
-//     return button;
-//     }
+    // change page
+    Click: function() {
+        Pagination.page = +this.innerHTML;
+        Pagination.Start();
+    },
 
-// DisplayList( movies, list_element, rows, current_page);
-// SetupPagination( movies, pagination_element, rows)
+    // previous page
+    Prev: function() {
+        Pagination.page--;
+        if (Pagination.page < 1) {
+            Pagination.page = 1;
+        }
+        Pagination.Start();
+    },
 
-
-const paginationElement = document.getElementById('pagination');
-const arrowLeft = document.querySelector('.arrow_left');
-const arrowRight = document.querySelector('.arrow_right');
-const searchFormRef = document.querySelector('.search-form');
-
-let currentPage = 1;
-let pageCount;
-const pagesOnWindow = 5;
-let rows = 20;
-
-function resetCurrentPage() {
-currentPage = 1;
- }
-
-
- 
- function render(totalPages, searchFormRef, callback, searchQuery) {
-  paginationElement.innerHTML = '';
-  resetCurrentPage();
-  arrowLeft.removeEventListener('click', onArrowLeftClick);
-  arrowRight.removeEventListener('click', onArrowRightClick);
-
-   function setupPagination(movies, wrapper) {
-     wrapper.innerHTML = '';
-     pageCount = pagenumbers;
-     let maxLeftPage = currentPage - Math.floor(pagesOnWindow / 2);
-     let maxRightPage = currentPage + Math.floor(pagesOnWindow / 2);
-
-     if (maxLeftPage < 1) {
-       maxLeftPage = 1;
-       maxRightPage = pagesOnWindow;
-     }
-
-     if (maxRightPage > totalPages) {
-       maxLeftPage = totalPages - (pagesOnWindow - 1);
-
-       if (maxLeftPage < 1) {
-         maxLeftPage = 1;
-       }
-       maxRightPage = totalPages;
-     }
-
-     for (let i = 1; i <= totalPages; i++) {
-       if (maxLeftPage !== 1 && i == 1) {
-         let btn = paginationButton(i, movies);
-         wrapper.appendChild(btn);
-       }
-
-       if (maxRightPage !== totalPages && i == totalPages) {
-         let btn = paginationButton(i, movies);
-         wrapper.appendChild(btn);
-       }
-
-       if (i >= maxLeftPage && i <= maxRightPage) {
-         let btn = paginationButton(i, movies);
-         wrapper.appendChild(btn);
-       }
-     }
-   }
+    // next page
+    Next: function() {
+        Pagination.page++;
+        if (Pagination.page > Pagination.size) {
+            Pagination.page = Pagination.size;
+        }
+        Pagination.Start();
+    },
 
 
 
-  function paginationButton(page) {
-    let button = document.createElement('button');
-    button.innerText = page;
 
-    if (currentPage == page) button.classList.add('active');
+    // binding pages
+    Bind: function() {
+        var a = Pagination.e.getElementsByTagName('a');
+        for (var i = 0; i < a.length; i++) {
+            if (+a[i].innerHTML === Pagination.page) a[i].className = 'current';
+            a[i].addEventListener('click', Pagination.Click, false);
+        }
+    },
 
-    button.addEventListener('click', function () {
-      currentPage = page;
-      callback(searchFormRef, currentPage);
+    // write pagination
+    Finish: function() {
+        Pagination.e.innerHTML = Pagination.code;
+        Pagination.code = '';
+        Pagination.Bind();
+    },
 
-      let current_btn = document.querySelector('.pagenumbers button.active');
-      current_btn.classList.remove('active');
+    // find pagination type
+  //   Start: function() {
+  //       if (Pagination.size < Pagination.step + 1) {
+  //           Pagination.Add(1, Pagination.size);
+  //       }
+  //       else if (Pagination.page < Pagination.step +2 ) {
+  //           Pagination.Add(1, Pagination.step +3);
+  //           Pagination.Last();
+  //       }
+  //       else if (Pagination.page >Pagination.size - Pagination.step) {
+  //           Pagination.First();
+  //           Pagination.Add(Pagination.size - Pagination.step* 2 - 2, Pagination.size+1 );
+  //       }
+  //       else {
+  //           Pagination.First();
+  //           Pagination.Add(Pagination.page - Pagination.step, Pagination.page+Pagination.step);
+  //           Pagination.Last();
+  //       }
+  //       Pagination.Finish();
+  // },
+    // find pagination type
+    Start: function() {
+        if (Pagination.size < Pagination.step * 2 + 6) {
+            Pagination.Add(1, Pagination.size + 1);
+        }
+        else if (Pagination.page < Pagination.step * 2 + 1) {
+            Pagination.Add(1, Pagination.step * 2 + 4);
+            Pagination.Last();
+        }
+        else if (Pagination.page > Pagination.size - Pagination.step * 2) {
+            Pagination.First();
+            Pagination.Add(Pagination.size - Pagination.step * 2 - 2, Pagination.size + 1);
+        }
+        else {
+            Pagination.First();
+            Pagination.Add(Pagination.page - Pagination.step, Pagination.page + Pagination.step + 1);
+            Pagination.Last();
+        }
+        Pagination.Finish();
+    },
+  
+    // binding buttons
+    Buttons: function(e) {
+        var nav = e.getElementsByTagName('a');
+      nav[0].addEventListener('click', Pagination.Prev, false);              //.onClick(a.incrementPage(-1));
+      nav[1].addEventListener('click', Pagination.Next, false);              //.onClick(a.incrementPage(1));
+    },
 
-      button.classList.add('active');
-      setupPagination(movies, paginationElement, rows);
-      hideExtremeButtons(totalPages);
+    // create skeleton
+    Create: function(e) {
+
+        var html = [
+            '<a class="arrows">&#8592;</a>', // previous button
+            '<span></span>',  // pagination container
+            '<a class="arrows">&#8594;</a>'  // next button
+        ];
+
+        e.innerHTML = html.join('');
+        Pagination.e = e.getElementsByTagName('span')[0];
+        Pagination.Buttons(e);
+    },
+
+    // init
+    Init: function(e, data) {
+        Pagination.Extend(data);
+        Pagination.Create(e);
+        Pagination.Start();
+    }
+};
+
+var init = function() {
+    Pagination.Init(document.getElementById('pagination'), {
+      size: 20, // pages size
+        page: 1,  // selected page
+        step: 2   // pages before and after current
     });
+};
 
-    return button;
-  }
-
-
-  //arrows function
-  function onArrowLeftClick() {
-    if (currentPage > 1) {
-      currentPage--;
-      setupPagination(movies, paginationElement, rows);
-      callback(searchFormRef, currentPage);
-    }
-
-    disableArrowBtn(totalPages);
-    hideExtremeButtons(totalPages);
-  }
-
-  function onArrowRightClick() {
-    if (currentPage < totalPages) {
-      currentPage++;
-      setupPagination(searchFormRef, paginationElement, rows);
-      callback(searchFormRef, currentPage);
-    }
-    inactiveArrow(totalPages);
-    hideExtremeButtons(totalPages);
-  }
-
-  setupPagination(movies, paginationElement, rows);
-  arrowLeft.onclick = onArrowLeftClick;
-  arrowRight.onclick = onArrowRightClick;
-
-  hideExtremeButtons(totalPages);
-  inactiveArrow(totalPages);
-}
-
-// inactive arrows
-function inactiveArrow(totalPages) {
-  if (currentPage === 1) {
-    arrowLeft.classList.add('disabled-arrow');
-  } else {
-    arrowLeft.classList.remove('disabled-arrow');
-  }
-
-  if (currentPage === totalPages) {
-    arrowRight.classList.add('disabled-arrow');
-  } else {
-    arrowRight.classList.remove('disabled-arrow');
-  }
-}
-
-
-// function fetchPopularFilmsByPage(page) {
-//   newApiService.pageNum = page;
-//   return newApiService.insertGenresToMovieObj();
-// }
-//  function fetchDataOfPopularFilms() {
-//   newApiService
-//     .fetchPopularArticlesPages()
-//     .then(results => {
-//       renderPagination(results.total_pages, results.results, displayList);
-//     })
-//     .catch(err => {
-//       console.log('error in function fetchDataOfPopularFilms');
-      
-//     });
-// }
-
-
+document.addEventListener('DOMContentLoaded', init, false);
