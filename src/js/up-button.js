51a71afe8c0595d6c5d80150c63692974ada836b
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-04-19 16:25:48 +0300

    up-button-fix-2

--- src/js/up-button.js
+++ src/js/up-button.js
@@ -1,16 +1,16 @@
-$(document).ready(function() { 
-    let button = $('#btn-scroll-up');	
-    $(window).scroll (function () {
-      if ($(this).scrollTop () > 300) {
-        button.fadeIn();
-      } else {
-        button.fadeOut();
-      }
-  });	 
-  button.on('click', function(){
-  $('body, html').animate({
-  scrollTop: 0
-  }, 0);
-  return false;
-  });		 
+$(document).ready(function () {
+  var button = $('#btn-scroll-up');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });
+  button.on('click', function () {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 100);
+    return false;
+  });
 });
